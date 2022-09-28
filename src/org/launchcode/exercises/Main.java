8dbb22d3b454a17a28b664033d5332586aa3f834
Sara Davis <smartinez.davis239@gmail.com>
2022-09-27 21:58:09 -0500

    exception exercises complete

--- src/org/launchcode/exercises/Main.java
+++ src/org/launchcode/exercises/Main.java
@@ -4,28 +4,60 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
+    private static int pointsEarned = 0;
 
     public static void main(String[] args) {
 
-        // Test out your Divide() function!
-
+//        int x = 10;
+//        int y = 0;
+//
+//            int grade = Divide(x, y);
         HashMap<String, String> studentFiles = new HashMap<>();
         studentFiles.put("Carl", "Program.java");
-        studentFiles.put("Brad", "");
-        studentFiles.put("Elizabeth", "MyCode.java");
+       studentFiles.put("Brad", "");
+        studentFiles.put("Elizabeth", "MyCode.pdf");
         studentFiles.put("Stefanie", "CoolProgram.java");
 
-        // Test out your CheckFileExtension() function!
+        for (Map.Entry<String, String> file : studentFiles.entrySet()){
+            System.out.println(file.getKey() + ": " + CheckFileExtension(file.getValue()));
+
+        }
+
     }
 
-    public static void Divide(int x, int y)
+    public static int Divide(int x, int y)
     {
-        // Write code here!
+
+        if (y == 0) {
+            try {
+                throw new ArithmeticException("Can't divide by zero");
+            } catch (ArithmeticException e) {
+                e.printStackTrace();
+            }
+        }
+
+       return x/y;
     }
 
-    public static int CheckFileExtension(String fileName)
-    {
-        // Write code here!
+    public static int CheckFileExtension(String fileName) {
+        if (fileName.equals("") || fileName == null) {
+            try {
+                throw new NullPointerException("this file needs an extension");
+            } catch (NullPointerException e) {
+                e.printStackTrace();
+            }
+            return pointsEarned - 1;
+        }
+        else if (fileName.endsWith(".java")) {
+            return pointsEarned +1;
+        }
+        else  {
+            return pointsEarned;
+
+        }
+
+
+
     }
 
 }
